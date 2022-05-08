ac8556cd0b65b59ab48072cd7becddeee0d4a79b
Dmitry <mdm@yandex.ru>
2022-05-08 17:59:37 +0300

    added function convert_SAjson2yolo

--- prepare_data_helper.py
+++ prepare_data_helper.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun May  8 10:07:09 2022
+# ver 07.05.22.2
 
-@author: Dmitry.Mokachev
-"""
-
-# ver 07.05.22.1
 import json
 import os
 import subprocess
 import re
+import numpy as np
 from IPython.display import clear_output
 
 def get_json_file(file_json: str):
@@ -83,5 +79,74 @@ def get_gdrive_file_by_link(gdrive_link, new_file_name, show_progress=True):
     return print(result)
 
 #update 08.05
-def new_func():
-    return True
+def xy2xywh(x1,x2,y1,y2):
+  x, y = (x1 + x2) / 2, (y1 + y2) / 2
+  w, h = x2 - x1, y2 - y1
+  return x, y, w, h
+
+def change_file_ext(fname, ext='txt'):
+  '''
+  Change extention of file name 'dir/file.jpg' -> 'dir/file.ext' default - ext=txt
+  :return: filename, string
+  '''
+  return ''.join(fname.split('.')[:-1])+'.'+ext
+
+def convert_SAjson2yolo(json_data, 
+                        original_img_dir, 
+                        train_yolo_dir, 
+                        train_val_dir='train',
+                        mono_class=False):
+  '''
+  convert Super annotate json data to yolo format and save
+  data must be obtained using the function get_json_file()
+  :param: train_val_dir - train or validation dir to save images and labels. Default = 'train'
+  :param: mono_class - True - all bbox class 0, False - original 
+  '''
+
+  # create train_data dir: train_yolo_dir/[images|labels]/train_val_dir
+  if os.path.exists(train_yolo_dir+'/images/'+'train') and os.path.exists(train_yolo_dir+'/labels/'+'train'):
+    print('folders', train_yolo_dir+'/[images|labels]/'+'train already exist!')
+  else:
+    os.makedirs(train_yolo_dir+'/images/'+train_val_dir, exist_ok=True)
+    os.makedirs(train_yolo_dir+'/labels/'+train_val_dir, exist_ok=True)
+    print('folders', train_yolo_dir+'/[images|labels]/'+'train created!')
+  
+  # convert 2 yolo
+  for file_id, file_name in enumerate(json_data):
+    if type(json_data[file_name]) == dict:
+      img_h, img_w = cv2.imread(original_img_dir+file_name).shape[:2]
+      yolo_data = []
+      for points_data in json_data[file_name]['instances']:
+        # convert absolute x1..y2 to relative (x,y) center and (w,h) bbox
+        x,y,w,h = xy2xywh(points_data['points']['x1']/img_w,
+                          points_data['points']['x2']/img_w,
+                          points_data['points']['y1']/img_h,
+                          points_data['points']['y2']/img_h)
+        
+        # print(file_name, (w,h), points_data)
+        if mono_class:
+          class_id = 0
+        else:
+          class_id = points_data['classId']
+
+        yolo_data.append([class_id, x, y, w, h])
+
+      # copy image and save labels txt
+      file_full_name_img = train_yolo_dir+'/images/'+train_val_dir+'/'+'img_{0:05d}.'.format(file_id)+file_name.split('.')[-1]
+      file_full_name_lbl = train_yolo_dir+'/labels/'+train_val_dir+'/'+'img_{0:05d}.txt'.format(file_id)
+      np.savetxt(file_full_name_lbl, yolo_data)
+
+      # save txt file with labels
+      with open(file_full_name_lbl, "w") as output:
+        output.write(str(yolo_data))
+
+      # copy image and change its file name
+      result = os.system(f'cp "{original_img_dir}/{file_name}" {file_full_name_img}')
+
+  # check result
+  len(json_data)-1
+  print(f'In json were - {len(json_data)-1} files')
+  print(f'Conderted - {file_id} files')
+  print('Saved images -', len(os.listdir(train_yolo_dir+'/images/'+train_val_dir)), 'files')
+  print('Saved labels -', len(os.listdir(train_yolo_dir+'/labels/'+train_val_dir)), 'files')
+
